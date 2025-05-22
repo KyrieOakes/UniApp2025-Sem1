b0ca2b7c12e56d67799ab62a76f4252c8f2073fa
mila-r10 <remilaaili10@gmail.com>
2025-05-22 19:56:56 +1000

    Update login_window.py

--- login_window.py
+++ login_window.py
@@ -10,21 +10,26 @@ def windows():
         em = email_in.get()
         pw = pw_in.get()
         if em == "" or pw == "":
+            # Show error popup if login fails
             messagebox.showerror("Oops", "Fill both fields plz")
             return
+            # Search the student by email from database
 
         student = db.get_student_by_email(em)
         if student and student.password == pw:
             root.destroy()
+            # If password is correct, login successful
             open_enrolment_window(student)
         else:
             messagebox.showerror("Nope", "Wrong info")
+            # Show error popup if login fails
 
     root = tk.Tk()
     root.title("Log in here")
     root.geometry("300x150")
 
     tk.Label(root, text="Email:").pack()
+    # Text entry box for entering email
     email_in = tk.Entry(root)
     email_in.pack()
 
